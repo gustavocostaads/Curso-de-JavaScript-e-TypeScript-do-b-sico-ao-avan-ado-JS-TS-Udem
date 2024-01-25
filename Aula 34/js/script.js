37fe7cf72fd4f2e4f803f2ce9c6e409eba13d162
Gustavo Costa Ribeiro Soares <gsoares@senado.leg.br>
2024-01-25 09:12:16 -0300

    continuação aula 34

--- Aula 34/js/script.js
+++ Aula 34/js/script.js
@@ -11,6 +11,18 @@ function meuEscopo() {
     const sobrenome = form.querySelector(".sobrenome");
     const peso = form.querySelector(".peso");
     const altura = form.querySelector(".altura");
+
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    });
+
+    console.log(pessoas);
+
+    resultado.innerHTML +=
+      `<p>${nome.value} ${sobrenome.value} ` + `${peso.value} ${altura.value}`;
   }
 
   form.addEventListener("submit", recebeEventoForm);
