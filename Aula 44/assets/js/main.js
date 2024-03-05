78ee3865ef2e2c54b5cb3008a09e26708fdc1768
Gustavo Costa Ribeiro Soares <gsoares@senado.leg.br>
2024-03-05 09:53:04 -0300

    revisão aula 44 2

--- Aula 44/assets/js/main.js
+++ Aula 44/assets/js/main.js
@@ -4,7 +4,6 @@ form.addEventListener("submit", function (e) {
   const inputPeso = e.target.querySelector("#peso");
   const inputAltura = e.target.querySelector("#altura");
 
-  
   const peso = Number(inputPeso.value)
   const altura = Number(inputAltura.value)
   console.log(peso, altura);
@@ -21,8 +20,36 @@ form.addEventListener("submit", function (e) {
 
   const imc = getImc(peso, altura)
   const nivelImc = getNivelImc(imc)
+  const msg = `Seu IMC é ${imc} (${nivelImc})`
 
-  const msg
-
-
+  setResultado(msg, true)
 });
+
+function getImc(peso, altura) {
+  const imc = peso / altura ** 2
+  return imc.toFixed(2)
+}
+
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado")
+  
+  if (isValid) {
+    p.classList.add('paragrafo-resultado')
+  } else {
+    p.classList.add('bad')
+  }
+  const p = document.createElement("p")
+  p.innerHTML = msg
+  resultado.appendChild(p)
+}
+
+function getNivelImc(imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+  if (imc < 18.5) return nivel[0]
+  if (imc <= 25) return nivel[1]
+  if (imc <= 30) return nivel[2]
+  if (imc <= 35) return nivel[3]
+  if (imc <= 40) return nivel[4]
+  if (imc > 40) return nivel[5]
+}
