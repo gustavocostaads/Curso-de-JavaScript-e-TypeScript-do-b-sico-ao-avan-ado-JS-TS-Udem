acb1458e27ed7cdc4f51e8e976c57978abf4f17c
Gustavo Costa Ribeiro Soares <gsoares@senado.leg.br>
2024-03-06 10:06:29 -0300

    estudo do exercício 44

--- Aula 44/assets/js/main.js
+++ Aula 44/assets/js/main.js
@@ -1,55 +1,34 @@
-const form = document.querySelector("#formulario");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const inputPeso = e.target.querySelector("#peso");
-  const inputAltura = e.target.querySelector("#altura");
+const form = document.querySelector("#formulario")
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault()
+
+  const inputPeso = e.target.querySelector("#peso")
+  const inputAltura = e.target.querySelector("#altura")
 
   const peso = Number(inputPeso.value)
   const altura = Number(inputAltura.value)
-  console.log(peso, altura);
 
-  if (!peso) {
-    setResultado("Peso Inválido", false)
-    return
-  }
+  console.log(peso, altura)
 
-  if (!altura) {
-    setResultado("Altura Inválida", false)
-    return
-  }
+  const imc = calculoImc(peso, altura)
 
-  const imc = getImc(peso, altura)
-  const nivelImc = getNivelImc(imc)
-  const msg = `Seu IMC é ${imc} (${nivelImc})`
+  const msg = `Seu IMC é ${imc}`
 
-  setResultado(msg, true)
-});
+  pushText(msg)
 
-function getImc(peso, altura) {
-  const imc = peso / altura ** 2
-  return imc.toFixed(2)
-}
+})
 
+function pushText(msg) {
+  const mensagem = document.querySelector("#resultado")
 
-function setResultado(msg, isValid) {
-  const resultado = document.querySelector("#resultado")
-  
-  if (isValid) {
-    p.classList.add('paragrafo-resultado')
-  } else {
-    p.classList.add('bad')
-  }
   const p = document.createElement("p")
-  p.innerHTML = msg
-  resultado.appendChild(p)
-}
 
-function getNivelImc(imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
-  if (imc < 18.5) return nivel[0]
-  if (imc <= 25) return nivel[1]
-  if (imc <= 30) return nivel[2]
-  if (imc <= 35) return nivel[3]
-  if (imc <= 40) return nivel[4]
-  if (imc > 40) return nivel[5]
+  p.innerHTML = msg
+  mensagem.appendChild(p)
 }
+  
+  function calculoImc (peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+  }
